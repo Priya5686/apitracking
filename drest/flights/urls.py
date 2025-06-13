8fa2d778aa2ddb06ab8a258f8c054e9b73528912
Priya Jay <priyaj5686@gmail.com>
2025-06-13 08:59:27 +0400

    error corrected

--- drest/flights/urls.py
+++ drest/flights/urls.py
@@ -3,7 +3,7 @@ from .views import FlightStatusAPIView, flight_form_view
 
 urlpatterns = [
     path('flight-form/', flight_form_view), 
-    path('api/flightstatus/',FlightStatusAPIView.as_view ),
+    path('api/flightstatus/',FlightStatusAPIView.as_view()),
 
 
 ]
