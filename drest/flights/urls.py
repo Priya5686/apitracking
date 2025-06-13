e0f5bb151f9ec0ededc72732ded2679bcd0ee717
Priya Jay <priyaj5686@gmail.com>
2025-06-13 08:42:54 +0400

    route updated

--- drest/flights/urls.py
+++ drest/flights/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import flight_status, flight_form_view
+from .views import FlightStatusAPIView, flight_form_view
 
 urlpatterns = [
     path('flight-form/', flight_form_view), 
-    path('api/flightstatus/', flight_status),
+    path('api/flightstatus/',FlightStatusAPIView.as_view ),
+
 
 ]
