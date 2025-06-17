fee568b834e282852bc0a1c648dbb536debce779
Priya Jay <priyaj5686@gmail.com>
2025-06-17 16:27:40 +0400

    delete unused url

--- drestapp/urls.py
+++ drestapp/urls.py
@@ -13,7 +13,7 @@ from .views import (
     AccountView,
     refresh_access_token,
     WhoAmIEndpoint,LogoutView, forgot_password_view, reset_password_view, GetAccessTokenView,
-    profile_view,WeatherAPIView,weather_view, oauth_success_redirect, login_page, GmailEventDetectionView,
+    profile_view,WeatherAPIView,weather_view, login_page, GmailEventDetectionView,
     extract_events_fallback, flight_data_view,fetch_stored_flights
 )
 
