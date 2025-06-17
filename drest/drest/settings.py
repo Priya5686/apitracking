31e00c55e50d422faf2307b3e6ca9c5b8690f39c
Priya Jay <priyaj5686@gmail.com>
2025-06-17 07:50:13 +0400

    bind port added in procfile

--- drest/drest/settings.py
+++ drest/drest/settings.py
@@ -133,7 +133,7 @@ INSTALLED_APPS = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://oauthapp-8izz.onrender.com"
+    "https://oauthapp-8izz.onrender.com",
 ]
 
 MIDDLEWARE = [
