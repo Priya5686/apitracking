974b0312d38f9a38d8c0fee095622b690f9ad1a2
Priya Jay <priyaj5686@gmail.com>
2025-06-17 20:48:57 +0400

    url added

--- flights/urls.py
+++ flights/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import flight_status, flight_form_view, save_subscription
+from .views import flight_status, flight_form_view, save_subscription, rapidapi_webhook
 
 urlpatterns = [
     path('flight-status/', flight_form_view), 
     path('api/flightstatus/',flight_status),
     path("api/save-subscription/", save_subscription),
-
+    path('api/rapidapi-webhook/', rapidapi_webhook),  
 ]
