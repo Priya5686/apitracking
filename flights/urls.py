3f9affd2ef0eb810348a96732f5c58fd236c2a7b
Priya Jay <priyaj5686@gmail.com>
2025-07-06 15:31:14 +0400

    url updated

--- flights/urls.py
+++ flights/urls.py
@@ -3,7 +3,7 @@ from .views import flight_status, flight_form_view, save_subscription, rapidapi_
 urlpatterns = [
     path('flight-status/', flight_form_view), 
     path('api/flightstatus/',flight_status),
-    path("api/save-subscription/", save_subscription),
-    path('api/rapidapi-webhook/', rapidapi_webhook),  
-    path('api/refresh-subscription/<uuid:subscription_id>/', refresh_subscription),
+    #path("api/save-subscription/", save_subscription),
+    #path('api/rapidapi-webhook/', rapidapi_webhook),  
+    #path('api/refresh-subscription/<uuid:subscription_id>/', refresh_subscription),
 ]
